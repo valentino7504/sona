23f6933760ed63a3ef30ef0830ca0b7c3192d709
Edwin Ade <edwinmbonyjr@gmail.com>
2026-02-28 22:41:59 +0000

    test(audio): add test code to play audio from my computer using interfaces

--- cmd/sona/main.go
+++ cmd/sona/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/valentino7504/sona/internal/audio"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	cmdArgs := os.Args
+	fileName := cmdArgs[1]
+	player, err := audio.NewAudioPlayer(fileName, "")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	player.Play()
 }
