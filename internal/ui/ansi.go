17b536f206bc3ab89750352f1f6a6c96cb3eba7e
Edwin Ade <edwinmbonyjr@gmail.com>
2026-03-05 20:33:28 +0000

    feat(ansi): ansi escape codes for various cursor and screen actions

---
+++ internal/ui/ansi.go
@@ -0,0 +1,40 @@
+package ui
+
+import "fmt"
+
+const CSI = "\033["
+
+func clearScreen() string {
+	return (CSI + "2J\033[H")
+}
+
+func showCursor(show bool) string {
+	if show {
+		return (CSI + "?25h")
+	}
+	return (CSI + "?25l")
+}
+
+func moveCursorUp(lines int) string {
+	return fmt.Sprintf(CSI+"%dA", lines)
+}
+
+func moveCursorDown(lines int) string {
+	return fmt.Sprintf(CSI+"%dB", lines)
+}
+
+func moveCursorRight(columns int) string {
+	return fmt.Sprintf(CSI+"%dC", columns)
+}
+
+func moveCursorLeft(columns int) string {
+	return fmt.Sprintf(CSI+"%dD", columns)
+}
+
+func cursorHome() string {
+	return (CSI + "H")
+}
+
+func moveCursorTo(x int, y int) string {
+	return fmt.Sprintf(CSI+"%d;%dH", x, y)
+}
