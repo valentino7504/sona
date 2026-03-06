da75f9778f1b14982323ee6aa2ed34dad61f448f
Edwin Ade <edwinmbonyjr@gmail.com>
2026-03-06 17:05:19 +0000

    feat(ui): initial ui setup with static elements and char reading

---
+++ internal/ui/ui.go
@@ -0,0 +1,51 @@
+package ui
+
+import (
+	"bufio"
+	"context"
+	"os"
+
+	"golang.org/x/term"
+)
+
+var prevState *term.State
+
+const (
+	CTRL_C = 3
+	CTRL_D = 4
+	SPC    = 32
+)
+
+func Start(cancel context.CancelFunc, input chan rune) {
+	var err error
+	prevState, err = term.MakeRaw(int(os.Stdin.Fd()))
+	if err != nil {
+		panic(err)
+	}
+	drawStatic()
+	defer Stop()
+	reader := bufio.NewReader(os.Stdin)
+	var char rune
+	for {
+		char, _, err = reader.ReadRune()
+		if err != nil {
+			continue
+		}
+		switch char {
+		case 'q', CTRL_C, CTRL_D:
+			// calling cancel() closes ctx.Done in main which unblocks and allows
+			// shutdown
+			cancel()
+			return
+		case 's', SPC:
+			input <- char
+		default:
+			continue
+		}
+	}
+}
+
+func Stop() {
+	resetCursor()
+	_ = term.Restore(int(os.Stdin.Fd()), prevState)
+}
