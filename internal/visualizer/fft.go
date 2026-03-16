b98337d0b641f6febeb7bece5c77e7541fdb5aca
Edwin Ade <edwinmbonyjr@gmail.com>
2026-03-16 12:09:59 +0000

    feat(fft): group fft values into bins for the frequency bars

--- internal/visualizer/fft.go
+++ internal/visualizer/fft.go
@@ -22,20 +22,42 @@ func applyHann(samples []float64) []float64 {
 	return windowed
 }
 
-// computeFFT computes fft for an array of samples. should be used after hann
+// computeFFTMags computes fft for an array of samples. should be used after hann
 // smoothing and overlapping (probably when I am able to get this working.
-func computeFFT(samples []float64) []complex128 {
+// It returns the magnitudes of the FFT values, since that is all I need for visualization.
+func computeFFTMags(samples []float64) []float64 {
 	n := len(samples)
 	fft := fourier.NewFFT(n)
-	coeff := fft.Coefficients(nil, samples)
-	return coeff
-}
-
-// getMagnitudes calculates the magnitude of the values of the FFT transform.
-func getMagnitudes(coeffs []complex128) []float64 {
+	coeffs := fft.Coefficients(nil, samples)
 	mags := make([]float64, len(coeffs))
 	for i, coeff := range coeffs {
 		mags[i] = cmplx.Abs(coeff)
 	}
 	return mags
+}
+
+// getBins is used to group the magnitude values into bins for the visualizer. I
+// am only using 16 bars to start out with.
+func getBins(vals []float64) []float64 {
+	boundaries := make([]float64, NO_BARS+1)
+	start := 1.0
+	end := float64(len(vals) - 1)
+	ratio := math.Pow((end / start), (1.0 / float64(NO_BARS)))
+	for i := range boundaries {
+		boundaries[i] = start * math.Pow(ratio, float64(i))
+	}
+	bins := make([]float64, NO_BARS)
+	for i := range bins {
+		binStart, binEnd := int(math.Floor(boundaries[i])), int(math.Floor(boundaries[i+1]))
+		sum := 0.0
+		for _, val := range vals[binStart:binEnd] {
+			sum += val
+		}
+		if binEnd != binStart {
+			bins[i] = sum / float64(binEnd-binStart)
+		} else {
+			bins[i] = 0
+		}
+	}
+	return bins
 }
